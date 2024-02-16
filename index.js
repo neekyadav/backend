f4c79d4333fc088481dbee27f6d7ef1aadd5b60d
NeerajKumar Yadav <ny755598@gmail.com>
2024-02-16 10:00:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("neek and coffee");
+
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/twitter", (req, res) => {
+  res.send('your twiter account is log in')
+}
+)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
